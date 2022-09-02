ae55a277d1a753086fda48ead7ef3b41136fff19
Spencer Berggreen <spencerberggreen@gmail.com>
2022-09-02 16:23:24 -0500

    add barbell weight section

--- Projects/Fitness Calculator/script.js
+++ Projects/Fitness Calculator/script.js
@@ -1,21 +1,20 @@
 document.getElementById('add-55').onclick = () => {
-    // document.querySelector() finds, and returns, the first element
-    // matching the supplied selector (or null, if no element is found):
+    // store plate positions as variables
     var el1 = document.querySelector('.red-left');
     var el2 = document.querySelector('.red-right');
 
-    // here we create an adjacent element from the string of HTML,
-    // the 'afterend' argument states that this adjacent element
-    // follows the el1 node, rather than preceding it or appearing
-    // within:
+    // create an element AFTER the plate position
     el1.insertAdjacentHTML('afterend', '<div class="l55"></div>');
     el2.insertAdjacentHTML('afterend', '<div class="r55"></div>');
 };
 
 document.getElementById('subtract-55').onclick = () => {
+    // create variables that specify which plates to remove
     var leftPlate = document.querySelector('.l55');
     var rightPlate = document.querySelector('.r55');
-
+    // delete the targeted plates
     leftPlate.remove();
     rightPlate.remove();
 };
+
+document.getElementById('barbell-weight').onclick = () => {};
