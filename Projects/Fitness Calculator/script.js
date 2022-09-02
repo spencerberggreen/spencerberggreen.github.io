1f82e89626aa4c91ebac67c665541e4f1aec6380
Spencer Berggreen <spencerberggreen@gmail.com>
2022-09-02 13:39:29 -0500

    create functioning 55 plate additions

--- Projects/Fitness Calculator/script.js
+++ Projects/Fitness Calculator/script.js
@@ -1,10 +1,13 @@
-// Get the button, and when the user clicks on it, execute myFunction
-document.getElementById('subtract-p55').onclick = function () {
-    myFunction();
-};
+document.getElementById('add-55').onclick = () => {
+    // document.querySelector() finds, and returns, the first element
+    // matching the supplied selector (or null, if no element is found):
+    var el1 = document.querySelector('.red-left');
+    var el2 = document.querySelector('.red-right');
 
-function myFunction() {
-    let div = document.createElement('div');
-    div.innerText = document.getElementById('getText').innerText;
-    document.body.appendChild(div);
-}
+    // here we create an adjacent element from the string of HTML,
+    // the 'afterend' argument states that this adjacent element
+    // follows the el1 node, rather than preceding it or appearing
+    // within:
+    el1.insertAdjacentHTML('afterend', '<div class="p55"></div>');
+    el2.insertAdjacentHTML('afterend', '<div class="p55"></div>');
+};
