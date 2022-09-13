c182a823fb8f12adfa23e225661c011189646775
Spencer Berggreen <spencerberggreen@gmail.com>
2022-09-13 10:06:07 -0500

    update submodule

--- Projects/barbell-calculator/script.js
+++ Projects/barbell-calculator/script.js
@@ -8,6 +8,14 @@ var yl = document.querySelector('.yellow-left');
 var yr = document.querySelector('.yellow-right');
 var gl = document.querySelector('.green-left');
 var gr = document.querySelector('.green-right');
+var blkl = document.querySelector('.black-left');
+var blkr = document.querySelector('.black-right');
+var wl = document.querySelector('.white-left');
+var wr = document.querySelector('.white-right');
+var sbl = document.querySelector('.sblue-left');
+var sbr = document.querySelector('.sblue-right');
+var sgl = document.querySelector('.sgreen-left');
+var sgr = document.querySelector('.sgreen-right');
 
 // weight
 let barbellWeight = 45;
@@ -109,6 +117,98 @@ document.getElementById('subtract-25').onclick = () => {
     rightPlate.remove();
     // subtract plates from total
     totalWeight -= 50;
+    updateWeight();
+};
+
+// 15 lbs (black)
+
+document.getElementById('add-15').onclick = () => {
+    // create an element AFTER the plate position
+    blkl.insertAdjacentHTML('afterend', '<div class="l15"></div>');
+    blkr.insertAdjacentHTML('afterend', '<div class="r15"></div>');
+    // add plates to total
+    totalWeight += 30;
+    updateWeight();
+};
+
+document.getElementById('subtract-15').onclick = () => {
+    // create variables that specify which plates to remove
+    var leftPlate = document.querySelector('.l15');
+    var rightPlate = document.querySelector('.r15');
+    // delete the targeted plates
+    leftPlate.remove();
+    rightPlate.remove();
+    // subtract plates from total
+    totalWeight -= 30;
+    updateWeight();
+};
+
+// 10 lbs (white)
+
+document.getElementById('add-10').onclick = () => {
+    // create an element AFTER the plate position
+    wl.insertAdjacentHTML('afterend', '<div class="l10"></div>');
+    wr.insertAdjacentHTML('afterend', '<div class="r10"></div>');
+    // add plates to total
+    totalWeight += 20;
+    updateWeight();
+};
+
+document.getElementById('subtract-10').onclick = () => {
+    // create variables that specify which plates to remove
+    var leftPlate = document.querySelector('.l10');
+    var rightPlate = document.querySelector('.r10');
+    // delete the targeted plates
+    leftPlate.remove();
+    rightPlate.remove();
+    // subtract plates from total
+    totalWeight -= 20;
+    updateWeight();
+};
+
+// 5 lbs (small blue)
+
+document.getElementById('add-5').onclick = () => {
+    // create an element AFTER the plate position
+    sbl.insertAdjacentHTML('afterend', '<div class="l5"></div>');
+    sbr.insertAdjacentHTML('afterend', '<div class="r5"></div>');
+    // add plates to total
+    totalWeight += 10;
+    updateWeight();
+};
+
+document.getElementById('subtract-5').onclick = () => {
+    // create variables that specify which plates to remove
+    var leftPlate = document.querySelector('.l5');
+    var rightPlate = document.querySelector('.r5');
+    // delete the targeted plates
+    leftPlate.remove();
+    rightPlate.remove();
+    // subtract plates from total
+    totalWeight -= 10;
+    updateWeight();
+};
+
+// 2.5 lbs (small green)
+
+document.getElementById('add-2-5').onclick = () => {
+    // create an element AFTER the plate position
+    sgl.insertAdjacentHTML('afterend', '<div class="l2-5"></div>');
+    sgr.insertAdjacentHTML('afterend', '<div class="r2-5"></div>');
+    // add plates to total
+    totalWeight += 5;
+    updateWeight();
+};
+
+document.getElementById('subtract-2-5').onclick = () => {
+    // create variables that specify which plates to remove
+    var leftPlate = document.querySelector('.l2-5');
+    var rightPlate = document.querySelector('.r2-5');
+    // delete the targeted plates
+    leftPlate.remove();
+    rightPlate.remove();
+    // subtract plates from total
+    totalWeight -= 5;
     updateWeight();
 };
 
